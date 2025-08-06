cdcd28707664541735a13c99e80324723bd4f364
gusta554 <ykonno26@gmail.com>
2025-08-05 22:50:33 -0300

    aula dia 05-08

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+
+      export default function Home(){
+        return(
+          <div>
+          <h1>Hello World!!</h1>
+          <p>Primeiro exemplo no Next</p>
+
+          <div className="quadrado"></div>
+          </div>
+          
+        );
+      }
+
+   
